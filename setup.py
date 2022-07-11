75f1a9e0fc5dc5fc02bf1c3c244d433f40cd866c
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2022-07-11 13:08:22 +0100

    automatic test fixed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import setuptools
 from setuptools import setup
 
-VERSION="0.0.2"
+VERSION="0.0.3"
 
 def readme():
   readmePath = os.path.abspath(os.path.join(__file__, "..", "README.md"))
